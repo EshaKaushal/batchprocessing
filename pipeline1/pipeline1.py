6bf3ec4d6f0794d0be62fc7a141d05aae2477e43
EshaKaushal <212esha@gmail.com>
2021-06-12 10:43:17 -0400

    Update pipeline1.py

--- pipeline1/pipeline1.py
+++ pipeline1/pipeline1.py
@@ -5,6 +5,10 @@ import logging
 from google.cloud import storage
 from transformers.pipelines import pipeline
 
+environment = ''
+bucket = ''
+models = {}
+delimiter='/'
 
 # function to read file from input directory
 # answer the questions and write it to output directory
@@ -23,7 +27,6 @@ def question_answer(qa_file):
         print("inside file reading loop")
         reader = csv.DictReader(file)
         print("file read complete")
-        print(reader)
         for row in reader:
             print("Inside File Row Read loop")
             print(row)
@@ -42,7 +45,10 @@ def question_answer(qa_file):
     #    questions.append(question)
     #    answer.append(hg_comp({'question': question, 'context': context})['answer'])
     timestamp = str(int(time.time()))
-    with open('/pfs/out/' + "question_answer_" + timestamp + ".csv", 'w') as f:
+    output_folder = os.getcwd() + '\pfs\out'
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder, mode=0o777)
+    with open(output_folder + delimiter+'answer_' + timestamp + ".csv", 'w') as f:
         fileWriter = csv.writer(f, delimiter=',')
         for row in zip(*final_answer):
             fileWriter.writerow(row)
@@ -52,26 +58,43 @@ def question_answer(qa_file):
 
 
 def downloadFiles():
-    logging.debug('Inside Download Files')
+
+    print('Inside Download Files')
+
+    bucket_name = 'mgmt590-assgn4'
+    storage_client = storage.Client.from_service_account_json('../test/credentials.json')
+    bucket = storage_client.get_bucket(bucket_name)
 
     # Create this folder locally if not exists
+    output_folder = os.getcwd()+'\pfs\in'
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder, mode=0o777)
     try:
         blobs = bucket.list_blobs()
         for blob in blobs:
-            blob.download_to_filename('/pfs/question/' + blob.name)
+            blob.download_to_filename(output_folder+delimiter+blob.name)
     except Exception as ex:
-        logging.error("Exception occurred while trying to download files ", ex)
+     print("Exception occurred while trying to download files ", ex)
 
 
 if __name__ == '__main__':
 
-    storage_client = storage.Client()
-    bucket = storage_client.get_bucket('mgmt590-prd-file-upload')
-    print("Downloading Files")
-    downloadFiles()
+    environment = "local"
 
+    if environment == "local":
+        # bucket_name = 'mgmt590-storage'
+        # storage_client = storage.Client.from_service_account_json('credentials.json')
+        # bucket = storage_client.get_bucket(bucket_name)
+        downloadFiles()
+    elif environment == "prod":
+        bucket_name = 'mgmt590-assgn4'
+        storage_client = storage.Client()
+        bucket = storage_client.get_bucket(bucket_name)
+        print("Downloading Files")
+        downloadFiles()
+    output_folder = os.getcwd()+'\pfs\in'
     # walk /pfs/question_answer and call question_answer on every file found
-    for dirpath, dirs, files in os.walk("/pfs/question"):
+    for dirpath, dirs, files in os.walk(output_folder):
         for file in files:
             print("We are looping in the files")
             print("File Name: " + file)
